6e2be30298d964884fe20daaab9561d91458f873
nagokos <kosnago0428@gmail.com>
2023-12-30 02:37:35 +0900

    add DiffViewClose key mapping

--- .config/nvim/lua/rc/pluginconfig/diffview.lua
+++ .config/nvim/lua/rc/pluginconfig/diffview.lua
@@ -1,1 +1,2 @@
 vim.api.nvim_set_keymap("n", "[_Git]d", "<Cmd>DiffviewOpen<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "q", "<Cmd>DiffviewClose<CR>", { noremap = true, silent = true })
