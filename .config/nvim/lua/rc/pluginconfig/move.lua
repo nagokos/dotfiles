4244891ccafa4b5749375f9ef10eb198476f07eb
naohiro kosuda <94012895+nagokos@users.noreply.github.com>
2024-02-01 19:12:30 +0900

    plugin setup

--- .config/nvim/lua/rc/pluginconfig/move.lua
+++ .config/nvim/lua/rc/pluginconfig/move.lua
@@ -1,3 +1,5 @@
+require("move").setup()
+
 vim.keymap.set("n", "<C-n>", ":MoveLine(1)<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-p>", ":MoveLine(-1)<CR>", { noremap = true, silent = true })
 vim.keymap.set("v", "<C-n>", ":MoveBlock(1)<CR>", { noremap = true, silent = true })
