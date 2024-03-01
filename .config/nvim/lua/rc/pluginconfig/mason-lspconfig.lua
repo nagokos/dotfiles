70fcbe810cebd626ba693c5659a27edc6edc2f44
naohiro kosuda <94012895+nagokos@users.noreply.github.com>
2024-03-01 23:22:14 +0900

    fix

--- .config/nvim/lua/rc/pluginconfig/mason-lspconfig.lua
+++ .config/nvim/lua/rc/pluginconfig/mason-lspconfig.lua
@@ -5,127 +5,127 @@ require("mason-lspconfig").setup()
 -- selene: allow(unused_variable)
 ---@diagnostic disable-next-line: unused-local
 local on_attach = function(client, bufnr)
-	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp.omnifunc", { buf = bufnr })
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp.omnifunc", { buf = bufnr })
 
-	-- Mappings.
-	local opts = { noremap = true, silent = true, buffer = bufnr }
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	vim.keymap.set("n", "?", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	vim.keymap.set("n", "g?", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-	vim.keymap.set("n", "[_Lsp]wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-	vim.keymap.set("n", "[_Lsp]wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-	vim.keymap.set("n", "[_Lsp]wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-	vim.keymap.set("n", "[_Lsp]D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-	vim.keymap.set("n", "[_Lsp]a", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-	vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	vim.keymap.set("n", "[_Lsp]e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-	vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-	vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-	vim.keymap.set("n", "[_Lsp]q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-	vim.keymap.set("n", "[_Lsp]f", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
+  -- Mappings.
+  local opts = { noremap = true, silent = true, buffer = bufnr }
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  vim.keymap.set("n", "?", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  vim.keymap.set("n", "g?", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  vim.keymap.set("n", "[_Lsp]wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+  vim.keymap.set("n", "[_Lsp]wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+  vim.keymap.set("n", "[_Lsp]wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+  vim.keymap.set("n", "[_Lsp]D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+  vim.keymap.set("n", "[_Lsp]a", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+  vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  vim.keymap.set("n", "[_Lsp]e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+  vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+  vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+  vim.keymap.set("n", "[_Lsp]q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+  vim.keymap.set("n", "[_Lsp]f", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
 
-	require("lspconfig.ui.windows").default_options.border = "rounded"
-	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-		silent = true,
-		border = "rounded",
-	})
-	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-		silent = true,
-		border = "rounded",
-	})
-	vim.lsp.handlers["testDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-		underline = true,
-		virtual_text = {
-			spacing = 5,
-			severity_limit = "Warning",
-		},
-		update_in_insert = true,
-	})
+  require("lspconfig.ui.windows").default_options.border = "rounded"
+  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+    silent = true,
+    border = "rounded",
+  })
+  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+    silent = true,
+    border = "rounded",
+  })
+  vim.lsp.handlers["testDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    virtual_text = {
+      spacing = 5,
+      severity_limit = "Warning",
+    },
+    update_in_insert = true,
+  })
 end
 
 local group_name = "vimrc_mason_lspconfig"
 vim.api.nvim_create_augroup(group_name, { clear = true })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(args)
-		local bufnr = args.buf
-		local client = vim.lsp.get_client_by_id(args.data.client_id)
-		if client.supports_method("textDocument/inlayHint") then
-			vim.lsp.inlay_hint.enable(bufnr, true)
-		end
-	end,
-	group = group_name,
+  callback = function(args)
+    local bufnr = args.buf
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client.supports_method("textDocument/inlayHint") then
+      vim.lsp.inlay_hint.enable(bufnr, true)
+    end
+  end,
+  group = group_name,
 })
 
 local lspconfig = require("lspconfig")
 local capabilities = vim.tbl_deep_extend(
-	"force",
-	vim.lsp.protocol.make_client_capabilities(),
-	require("cmp_nvim_lsp").default_capabilities()
+  "force",
+  vim.lsp.protocol.make_client_capabilities(),
+  require("cmp_nvim_lsp").default_capabilities()
 )
 require("mason-lspconfig").setup_handlers({
-	function(server_name)
-		lspconfig[server_name].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-	end,
-	["rust_analyzer"] = function()
-		lspconfig.rust_analyzer.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = {
-				check = {
-					command = "clippy",
-				},
-				diagnostics = {
-					experimental = {
-						enable = true,
-					},
-				},
-			},
-		})
-	end,
-	["tsserver"] = function()
-		lspconfig.tsserver.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = {
-				completions = {
-					completeFunctionCalls = true,
-				},
-			},
-		})
-	end,
-	["clangd"] = function()
-		lspconfig.clangd.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-	end,
-	["lua_ls"] = function()
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-					},
-					-- hint = { enable = true },
-					format = {
-						enable = true,
-						defaultConfig = {
-							indent_style = "tab",
-							indent_size = "2",
-						},
-					},
-				},
-			},
-		})
-	end,
+  function(server_name)
+    lspconfig[server_name].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+  end,
+  ["rust_analyzer"] = function()
+    lspconfig.rust_analyzer.setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = {
+        check = {
+          command = "clippy",
+        },
+        diagnostics = {
+          experimental = {
+            enable = true,
+          },
+        },
+      },
+    })
+  end,
+  ["tsserver"] = function()
+    lspconfig.tsserver.setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = {
+        completions = {
+          completeFunctionCalls = true,
+        },
+      },
+    })
+  end,
+  ["clangd"] = function()
+    lspconfig.clangd.setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+  end,
+  ["lua_ls"] = function()
+    lspconfig.lua_ls.setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+          -- hint = { enable = true },
+          format = {
+            enable = true,
+            defaultConfig = {
+              indent_style = "tab",
+              indent_size = "2",
+            },
+          },
+        },
+      },
+    })
+  end,
 })
--- .config/nvim/lua/rc/pluginconfig/nvim-dap.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-dap.lua
@@ -1,10 +1,10 @@
 local dap = require("dap")
 dap.adapters.rust = {
-	type = "executable",
-	attach = { pidProperty = "pid", pidSelect = "ask" },
-	command = "lldb-vscode", -- my binary was called 'lldb-vscode-11'
-	env = { LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = "YES" },
-	name = "lldb",
+  type = "executable",
+  attach = { pidProperty = "pid", pidSelect = "ask" },
+  command = "lldb-vscode", -- my binary was called 'lldb-vscode-11'
+  env = { LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = "YES" },
+  name = "lldb",
 }
 
 -- dap.configurations.rust = {
@@ -17,15 +17,15 @@ dap.adapters.rust = {
 -- }
 
 dap.configurations.lua = {
-	{
-		type = "nlua",
-		request = "attach",
-		name = "Attach to running Neovim instance",
-	},
+  {
+    type = "nlua",
+    request = "attach",
+    name = "Attach to running Neovim instance",
+  },
 }
 
 dap.adapters.nlua = function(callback, config)
-	callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
+  callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
 end
 
 vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "", linehl = "", numhl = "" })
@@ -34,10 +34,10 @@ vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "", linehl = "", n
 vim.api.nvim_set_keymap("n", "[_Debugger]", "<Nop>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "z", "[_Debugger]", {})
 vim.api.nvim_set_keymap(
-	"n",
-	"[_Debugger]b",
-	"<Cmd>lua require'dap'.toggle_breakpoint()<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "[_Debugger]b",
+  "<Cmd>lua require'dap'.toggle_breakpoint()<CR>",
+  { noremap = true, silent = true }
 )
 vim.api.nvim_set_keymap("n", "[_Debugger]c", "<Cmd>lua require'dap'.continue()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "[_Debugger]n", "<Cmd>lua require'dap'.step_over()<CR>", { noremap = true, silent = true })
@@ -45,46 +45,46 @@ vim.api.nvim_set_keymap("n", "[_Debugger]i", "<Cmd>lua require'dap'.step_into()<
 vim.api.nvim_set_keymap("n", "[_Debugger]o", "<Cmd>lua require'dap'.step_out()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "[_Debugger]R", "<Cmd>lua require'dap'.repl.open()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap(
-	"n",
-	"[_Debugger]B",
-	"<Cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "[_Debugger]B",
+  "<Cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+  { noremap = true, silent = true }
 )
 vim.api.nvim_set_keymap(
-	"n",
-	"[_Debugger]l",
-	"<Cmd>lua require'dap'.load_launchjs()<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "[_Debugger]l",
+  "<Cmd>lua require'dap'.load_launchjs()<CR>",
+  { noremap = true, silent = true }
 )
 vim.api.nvim_set_keymap("n", "[_Debugger]g", "<Cmd>lua require'dap'.run_last()<CR>", { noremap = true, silent = true })
 -- telescope
 vim.api.nvim_set_keymap(
-	"n",
-	"[_Debugger]H",
-	"<Cmd>lua require'telescope'.extensions.dap.commands{}<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "[_Debugger]H",
+  "<Cmd>lua require'telescope'.extensions.dap.commands{}<CR>",
+  { noremap = true, silent = true }
 )
 vim.api.nvim_set_keymap(
-	"n",
-	"[_Debugger]C",
-	"<Cmd>lua require'telescope'.extensions.dap.configurations{}<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "[_Debugger]C",
+  "<Cmd>lua require'telescope'.extensions.dap.configurations{}<CR>",
+  { noremap = true, silent = true }
 )
 vim.api.nvim_set_keymap(
-	"n",
-	"[_Debugger]P",
-	"<Cmd>lua require'telescope'.extensions.dap.list_breakpoints{}<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "[_Debugger]P",
+  "<Cmd>lua require'telescope'.extensions.dap.list_breakpoints{}<CR>",
+  { noremap = true, silent = true }
 )
 vim.api.nvim_set_keymap(
-	"n",
-	"[_Debugger]V",
-	"<Cmd>lua require'telescope'.extensions.dap.variables{}<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "[_Debugger]V",
+  "<Cmd>lua require'telescope'.extensions.dap.variables{}<CR>",
+  { noremap = true, silent = true }
 )
 vim.api.nvim_set_keymap(
-	"n",
-	"[_Debugger]q",
-	"<Cmd>lua require'dap'.disconnect({})<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "[_Debugger]q",
+  "<Cmd>lua require'dap'.disconnect({})<CR>",
+  { noremap = true, silent = true }
 )
