8752b75a91bd9573d72c6ec4550ed6602c80bd75
naohiro kosuda <94012895+nagokos@users.noreply.github.com>
2024-06-23 23:09:06 +0900

    change font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -62,8 +62,8 @@ local local_config = load_local_config("local")
 --- Config
 ---------------------------------------------------------------
 local config = {
-  font = wezterm.font("MonaspiceNe Nerd Font", { weight = "Light", stretch = "Normal", style = "Normal" }),
-  -- harfbuzz_features = { "ss02" },
+  font = wezterm.font("MonaspiceAr Nerd Font", { weight = "Light", stretch = "Normal", style = "Normal" }),
+  harfbuzz_features = { "ss02" },
   font_size = 21.0,
   check_for_updates = false,
   use_ime = true,
