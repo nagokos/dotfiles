97f74dd6c24c495535cddfc72205e4f56737127e
naohiro kosuda <94012895+nagokos@users.noreply.github.com>
2024-07-23 11:03:22 +0900

    update config

--- .config/nvim/lua/rc/mappings.lua
+++ .config/nvim/lua/rc/mappings.lua
@@ -38,7 +38,6 @@ vim.keymap.set("n", "gm", "<Nop>", { noremap = true, silent = true })
 vim.keymap.set("n", "go", "<Nop>", { noremap = true, silent = true })
 vim.keymap.set("n", "gq", "<Nop>", { noremap = true, silent = true })
 vim.keymap.set("n", "gr", "<Nop>", { noremap = true, silent = true })
--- vim.keymap.set("n", "gs", "<Nop>", { noremap = true, silent = true })
 vim.keymap.set("n", "gw", "<Nop>", { noremap = true, silent = true })
 vim.keymap.set("n", "g^", "<Nop>", { noremap = true, silent = true })
 vim.keymap.set("n", "g?", "<Nop>", { noremap = true, silent = true })
@@ -52,40 +51,35 @@ vim.keymap.set("n", "gk", "k", { noremap = true, silent = true })
 
 -- move cursor
 vim.keymap.set({ "n", "x" }, "j", function()
-  return vim.v.count > 0 and "j" or "gj"
+	return vim.v.count > 0 and "j" or "gj"
 end, { noremap = true, expr = true })
 vim.keymap.set({ "n", "x" }, "k", function()
-  return vim.v.count > 0 and "k" or "gk"
+	return vim.v.count > 0 and "k" or "gk"
 end, { noremap = true, expr = true })
 
 -- Automatically indent with i and A made by ycino
 vim.keymap.set("n", "i", function()
-  return string.len(vim.api.nvim_get_current_line()) ~= 0 and "i" or '"_cc'
+	return string.len(vim.api.nvim_get_current_line()) ~= 0 and "i" or '"_cc'
 end, { noremap = true, expr = true, silent = true })
 vim.keymap.set("n", "A", function()
-  return string.len(vim.api.nvim_get_current_line()) ~= 0 and "A" or '"_cc'
+	return string.len(vim.api.nvim_get_current_line()) ~= 0 and "A" or '"_cc'
 end, { noremap = true, expr = true, silent = true })
 
 -- toggle 0, ^ made by ycino
 vim.keymap.set("n", "0", function()
-  return string.match(vim.api.nvim_get_current_line():sub(0, vim.api.nvim_win_get_cursor(0)[2]), "^%s+$") and "0"
-      or "^"
+	return string.match(vim.api.nvim_get_current_line():sub(0, vim.api.nvim_win_get_cursor(0)[2]), "^%s+$") and "0"
+			or "^"
 end, { noremap = true, expr = true, silent = true })
 
 -- Emacs style
 vim.keymap.set("c", "<C-a>", "<Home>", { noremap = true, silent = false })
 if not vim.g.vscode then
-  vim.keymap.set("c", "<C-e>", "<End>", { noremap = true, silent = false })
+	vim.keymap.set("c", "<C-e>", "<End>", { noremap = true, silent = false })
 end
-vim.keymap.set("c", "<C-f>", "<right>", { noremap = true, silent = false })
-vim.keymap.set("c", "<C-b>", "<left>", { noremap = true, silent = false })
-vim.keymap.set("c", "<C-d>", "<DEL>", { noremap = true, silent = false })
--- vim.keymap.set('c', '<C-h>', '<BS>', {noremap = true, silent = true})
-vim.keymap.set("c", "<C-s>", "<BS>", { noremap = true, silent = false })
+vim.keymap.set("c", "<C-l>", "<right>", { noremap = true, silent = false })
+vim.keymap.set("c", "<C-h>", "<left>", { noremap = true, silent = false })
 vim.keymap.set("i", "<C-a>", "<Home>", { noremap = true, silent = false })
 vim.keymap.set("i", "<C-e>", "<End>", { noremap = true, silent = false })
-vim.keymap.set("i", "<C-f>", "<right>", { noremap = true, silent = false })
-vim.keymap.set("i", "<C-b>", "<left>", { noremap = true, silent = false })
 vim.keymap.set("i", "<C-h>", "<left>", { noremap = true, silent = false })
 vim.keymap.set("i", "<C-l>", "<right>", { noremap = true, silent = false })
 vim.keymap.set("i", "<C-k>", "<up>", { noremap = true, silent = false })
@@ -99,30 +93,6 @@ vim.keymap.set("n", "gL", "L", { noremap = true, silent = true })
 -- Clear highlighting
 vim.keymap.set("n", "gq", "<Cmd>nohlsearch<CR>", { noremap = true, silent = true })
 
--- lambdalisue's yank for slack
-vim.keymap.set("x", "[_SubLeader]y", function()
-  vim.cmd.normal({ "y", bang = true })
-  local content = vim.fn.getreg(vim.v.register, 1, true)
-  local spaces = {}
-  for _, v in ipairs(content) do
-    table.insert(spaces, string.match(v, "%s*"):len())
-  end
-  table.sort(spaces)
-  local leading = spaces[1]
-  local content_new = {}
-  for _, v in ipairs(content) do
-    table.insert(content_new, string.sub(v, leading + 1))
-  end
-  vim.fn.setreg(vim.v.register, content_new, vim.fn.getregtype(vim.v.register))
-end, { noremap = true, silent = true })
-
--- paste
-vim.keymap.set({ "n", "x" }, "p", "]p", { noremap = true, silent = true })
-vim.keymap.set({ "n", "x" }, "gp", "p", { noremap = true, silent = true })
-vim.keymap.set({ "n", "x" }, "gP", "P", { noremap = true, silent = true })
-vim.keymap.set({ "n", "x" }, "<LocalLeader>p", '"+p', { noremap = true, silent = true })
-vim.keymap.set({ "n", "x" }, "<LocalLeader>P", '"+P', { noremap = true, silent = true })
-
 -- Increment / Decrement
 vim.keymap.set({ "n", "x" }, "+", "<C-a>", { noremap = true, silent = true })
 vim.keymap.set({ "n", "x" }, "_", "<C-x>", { noremap = true, silent = true })
@@ -132,19 +102,15 @@ vim.keymap.set("n", "[_SubLeader]q", "<Cmd>copen<CR>", { noremap = true, silent 
 vim.keymap.set("n", "[_SubLeader]l", "<Cmd>lopen<CR>", { noremap = true, silent = true })
 
 -- For search
-vim.keymap.set("n", "g/", "/\\v", { noremap = true, silent = false })
 vim.keymap.set("n", "*", "g*N", { noremap = true, silent = true })
 vim.keymap.set("x", "*", 'y/<C-R>"<CR>N', { noremap = true, silent = true })
 
 -- noremap # g#n
-vim.keymap.set({ "n", "x" }, "g*", "*N", { noremap = true, silent = true })
-vim.keymap.set({ "n", "x" }, "g#", "#n", { noremap = true, silent = true })
 vim.keymap.set("x", "/", "<ESC>/\\%V", { noremap = true, silent = false })
 vim.keymap.set("x", "?", "<ESC>?\\%V", { noremap = true, silent = false })
 
 -- For replace
 vim.keymap.set("n", "gr", "gd[{V%::s/<C-R>///gc<left><left><left>", { noremap = true, silent = false })
-vim.keymap.set("n", "gR", "gD:%s/<C-R>///gc<left><left><left>", { noremap = true, silent = false })
 vim.keymap.set("n", "[_SubLeader]s", ":%s/\\<<C-r><C-w>\\>/", { noremap = true, silent = false })
 vim.keymap.set("x", "[_SubLeader]s", ":s/\\%V", { noremap = true, silent = false })
 
