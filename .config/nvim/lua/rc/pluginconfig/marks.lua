58338a6522d5cb2e30ba9f0afcd14c49428a6c1c
naohiro kosuda <94012895+nagokos@users.noreply.github.com>
2024-09-10 14:31:01 +0900

    delete files

--- .config/nvim/lua/rc/pluginconfig/marks.lua
+++
@@ -1,4 +0,0 @@
-require("marks").setup({
-  default_mappings = false, -- whether to map keybinds or not. default true
-  builtin_marks = {},       -- which builtin marks to show. default {}
-})
--- .config/nvim/lua/rc/pluginconfig/nvim-spectre.lua
+++
@@ -1,3 +0,0 @@
-require("spectre").setup()
-
-vim.cmd("command! Grep lua require('spectre').open()")
