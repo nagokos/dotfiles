257bc18a7f3ca44931b34c74ff4be6d9ca121c0a
naohiro kosuda <kosnago0428@gmail.com>
2024-09-29 14:53:36 +0900

    update settings

--- darwin/configutation.nix
+++ darwin/configutation.nix
@@ -5,15 +5,23 @@
     NSGlobalDomain = {
       InitialKeyRepeat = 10;
       KeyRepeat = 1;
-      AppleInterfaceStyle = "Dark";
-      _HIHideMenuBar = true;
+
+      AppleShowAllExtensions = true;
       AppleKeyboardUIMode = 3;
+      AppleInterfaceStyle = "Dark";
+
+      _HIHideMenuBar = true;
+
       NSAutomaticCapitalizationEnabled = false;
       NSAutomaticDashSubstitutionEnabled = false;
       NSAutomaticPeriodSubstitutionEnabled = false;
       NSAutomaticQuoteSubstitutionEnabled = false;
       NSAutomaticSpellingCorrectionEnabled = false;
       NSAutomaticWindowAnimationsEnabled = false;
+    };
+    finder = {
+      AppleShowAllFiles = true;
+      AppleShowAllExtensions = true;
     };
     dock = {
       autohide = true;
