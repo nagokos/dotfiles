c28c56c95fb0c4742c25868952a283b8efa947fc
naohiro kosuda <kosnago0428@gmail.com>
2025-11-03 09:45:29 +0900

    fix git settings

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -13,36 +13,45 @@ in
   programs = {
     git = {
       enable = true;
-      userName = "naohiro kosuda";
-      userEmail = "kosnago0428@gmail.com";
+      settings = {
+        user = {
+          name = "naohiro kosuda";
+          email = "kosnago0428@gmail.com";
+        };
 
-      delta = {
-        enable = true;
-        options = {
-          features = "side-by-side line-numbers decorations";
-          decorations = {
-            commit-decoration-style = "bold yellow box ul";
-            file-style = "bold yellow";
-            file-decoration-style = "none";
+        alias = {
+          ap = "add - p";
+          ba = "branch -a";
+          s = "switch";
+          sm = "switch -";
+          sc = "switch -c";
+          cm = "commit -m";
+          fixit = "commit --amend --no-edit";
+          st = "status";
+          f = "fetch";
+          r = "rebase";
+          lo = "log --oneline";
+          po = "push origin HEAD";
+          df = "diff";
+          dfs = "diff --staged";
+        };
+
+        delta = {
+          enable = true;
+          options = {
+            features = "side-by-side line-numbers decorations";
+            # decorations = {
+            #   commit-decoration-style = "bold yellow box ul";
+            #   file-style = "bold yellow";
+            #   file-decoration-style = "none";
+            # };
           };
         };
-      };
 
-      aliases = {
-        ap = "add - p";
-        ba = "branch -a";
-        s = "switch";
-        sm = "switch -";
-        sc = "switch -c";
-        cm = "commit -m";
-        fixit = "commit --amend --no-edit";
-        st = "status";
-        f = "fetch";
-        r = "rebase";
-        lo = "log --oneline";
-        po = "push origin HEAD";
-        df = "diff";
-        dfs = "diff --staged";
+        # extraConfig
+        init.defaultBranch = "main";
+        merge.confilictstyle = "diff3";
+        credential.helper = "osxkeychain";
       };
 
       ignores = lib.flatten [
@@ -51,11 +60,6 @@ in
         "*.log"
         gitignore_global
       ];
-
-      extraConfig = {
-        init.defaultBranch = "main";
-        merge.confilictstyle = "diff3";
-      };
     };
 
     gh = {
