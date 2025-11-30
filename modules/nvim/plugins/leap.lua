4aba558f50330adda0fa43203ee8aaa2ba82a0cc
naohiro kosuda <kosnago0428@gmail.com>
2025-11-30 22:27:12 +0900

    fix: leap.nvim setting

--- modules/nvim/plugins/leap.lua
+++ modules/nvim/plugins/leap.lua
@@ -2,11 +2,6 @@ require("lz.n").load({
 	"leap.nvim",
 	event = "DeferredUIEnter",
 	after = function()
-		vim.keymap.set("n", "s", function()
-			local focusable_windows = vim.tbl_filter(function(win)
-				return vim.api.nvim_win_get_config(win).focusable
-			end, vim.api.nvim_tabpage_list_wins(0))
-			require("leap").leap({ target_windows = focusable_windows })
-		end)
+		vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
 	end,
 })
