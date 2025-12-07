5f4e4e80020c981589303fc88cb3085510e87c80
naohiro kosuda <kosnago0428@gmail.com>
2025-12-07 22:21:16 +0900

    fix: quickfix keymap

--- modules/nvim/plugins/nvim-bqf.lua
+++ modules/nvim/plugins/nvim-bqf.lua
@@ -8,5 +8,13 @@ require("lz.n").load({
 				pscrolldown = "<C-d>",
 			},
 		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "qf",
+			callback = function()
+				vim.keymap.set("n", "q", "<Cmd>cclose<CR>", { buffer = true })
+			end,
+			group = vim.api.nvim_create_augroup("bqfKeymap", { clear = true }),
+		})
 	end,
 })
